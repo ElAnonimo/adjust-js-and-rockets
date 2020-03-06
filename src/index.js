34a7fdaf8644efcbc2519b7dc59930aa5f602266
el_anonimo <mikhail.gakh@aisa.ru>
2020-03-06 17:33:48 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 // Please run your solution from this file
 
+async function getValues() {
+	const res = await fetch("https://api.spacexdata.com/v3/launches/past");
+	const data = await res.json();
+	const values = data
+		.filter(x =>
+			x.launch_year === "2018" &&
+			x.rocket.second_stage.payloads.some(payload => payload.customers.some(customer => customer.includes("NASA")))
+		)
+		.map(x => {
+			const {
+				flight_number,
+				mission_name,
+				rocket: { second_stage }
+			} = x;
+
+			return {
+				flight_number,
+				mission_name,
+				payloads_count: second_stage.payloads.length
+			};
+		})
+		.sort((a, b) => new Date(a.launch_date_utc).getTime() < new Date(b.launch_date_utc).getTime() ? -1 : 1)
+  	.sort((a, b) => b.payloads_count - a.payloads_count);
+
+	return values;
+}
+
+(async function() {
+	const values = await getValues();
+	document.getElementById("out").innerHTML = JSON.stringify(values, null, 2);
+})();
+
 console.log("Hello from %csrc/index.js", "font-weight:bold");
--- src/solution.js
+++ src/solution.js
@@ -2,7 +2,37 @@
 
 const noop = () => {};
 
+const prepareData = data => {
+  const values = data
+  .filter(x =>
+    x.launch_year === "2018" &&
+    x.rocket.second_stage.payloads.some(payload => payload.customers.some(customer => customer.includes("NASA")))
+  )
+  .map(x => {
+    const {
+      flight_number,
+      mission_name,
+      rocket: { second_stage }
+    } = x;
+
+    return {
+      flight_number,
+      mission_name,
+      payloads_count: second_stage.payloads.length
+    };
+  })
+  .sort((a, b) => new Date(a.launch_date_utc).getTime() < new Date(b.launch_date_utc).getTime() ? -1 : 1)
+  .sort((a, b) => b.payloads_count - a.payloads_count);
+
+  return values;
+};
+
+const renderData = jsonData => {
+  document.body.innerHTML = `<pre id="out"></pre>`;
+  document.getElementById("out").innerHTML = JSON.stringify(jsonData, null, 2);
+};
+
 module.exports = {
-  prepareData: noop,
-  renderData: noop
+  prepareData,
+  renderData
 };
